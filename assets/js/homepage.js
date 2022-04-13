362ef344920c26341edb99b43cfff8295df3da49
Thunnder <gnorzea@gmail.com>
2022-04-13 17:56:02 -0400

    add complex queries

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function (user) {
     // format the github api url
@@ -18,10 +18,10 @@ var getUserRepos = function (user) {
             alert("Error: GitHub User Not Found");
         }
     })
-    .catch(function(error) {
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to GitHub");
-      });
+        .catch(function (error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to GitHub");
+        });
 };
 
 var displayRepos = function (repos, searchTerm) {
@@ -83,5 +83,31 @@ var formSubmitHandler = function (event) {
     console.log(event);
 };
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        displayRepos(data.items, language);
+                    });
+            } else {
+                alert('Error: GitHub User Not Found');
+            }
+        });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+    if(language) {
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent="";
+    }
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
